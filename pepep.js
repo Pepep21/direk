cf3efda1b3aa16de3c0608758a430a9f7b88fb81
Pepep21 <146739403+Pepep21@users.noreply.github.com>
2023-10-02 21:04:20 +0700

    Create pepep.js

---
+++ pepep.js
@@ -0,0 +1,12 @@
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://www.highcpmrevenuegate.com/zphvebbzh?key=b3be47ef4c8f10836b76435c09e7184f', '_blank');
+	});
